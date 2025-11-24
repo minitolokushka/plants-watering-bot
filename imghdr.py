3ec35f3143e234e08c7e866a0f68e738fc953489
minitolokushka <165062626+minitolokushka@users.noreply.github.com>
2025-11-24 21:08:53 +0300

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,6 @@
+# Простая заглушка для imghdr под Python 3.13,
+# чтобы старый python-telegram-bot не падал.
+# Боту для текста и кнопок этого достаточно.
+
+def what(file, h=None):
+    return None
